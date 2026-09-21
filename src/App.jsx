33dd3e21525d28feda4aa724b041d994d12e3dd1
ZEi <dhiazrusyda@gmail.com>
2026-09-21 11:35:24 +0700

    Add logout handling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ import Dashboard from "./components/pages/Dashboard";
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  function handleLogout() {
+    setIsLoggedIn(false); // ini yang bikin balik ke halaman Login
+  }
+
   if (!isLoggedIn) {
     return <Login onLoginSuccess={() => setIsLoggedIn(true)} />;
   }
 
-  return <Dashboard />;
+  return <Dashboard onLogout={handleLogout} />;
 }
